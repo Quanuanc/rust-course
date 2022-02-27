2787875da84583ccb89c243dc6a817fbb4b7823c
Quanuanc <quanuanc@outlook.com>
2022-02-27 11:52:06 +0800

    rust 基本概念

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 10;
+    let b: i32 = 20;
+    let mut c = 30i32; //可变变量
+    let d = 30_i32;
+    let e = add(add(a, b), add(c, d));
+    println!("(a+b)+(c+d)={}", e);
+}
+
+fn add(i: i32, j: i32) -> i32 {
+    i + j //没有分号，返回
 }
