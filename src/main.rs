53dd1fe3d296654704b3851f7b4966af439b85b3
Quanuanc <quanuanc@outlook.com>
2022-02-28 11:32:00 +0800

    match 1

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,15 @@
 fn main() {
-    let a = [1, 2, 3, 4, 5];
-    if a[2] > 3 {
-        println!(">");
-    } else if a[2] < 3 {
-        println!("<")
-    } else {
-        println!("=");
+    let dire = Direction::South;
+    match dire {
+        Direction::East => println!("East"),
+        Direction::North | Direction::South => println!("South or North"),
+        _ => println!("West"),
     }
+}
 
-    for i in a {
-        print!("{} ", i);
-    }
-
-    println!();
-
-    let mut i = 0;
-    while i < 5 {
-        print!("{} ", a[i]);
-        i += 1;
-    }
-    println!();
-    // loop 无条件循环
-    let mut ii = 0;
-    loop {
-        print!("{} ", ii);
-        ii += 1;
-        if ii == 10 {
-            break;
-        }
-    }
+enum Direction {
+    East,
+    West,
+    North,
+    South,
 }
