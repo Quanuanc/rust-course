8ea0b2a2d7b6be3b340df823ee3bd4ae498817fc
Quanuanc <quanuanc@outlook.com>
2022-02-28 15:53:46 +0800

    matches宏

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
 fn main() {
-    let actions = [
-        Action::Say("Hello Rust".to_string()),
-        Action::MoveTo(1, 2),
-        Action::ChangeColorRGB(1, 2, 3),
-    ];
-    for action in actions {
-        match action {
-            Action::Say(s) => {
-                println!("{}", s);
-            }
-            Action::MoveTo(x, y) => {
-                println!("{}, {}", x, y);
-            }
-            Action::ChangeColorRGB(r, g, b) => {
-                println!("{}, {}, {}", r, g, b);
-            }
-        }
-    }
-}
+    let foo = 'f';
+    assert!(matches!(foo, 'A'..='Z' | 'a'..='z'));
 
-enum Action {
-    Say(String),
-    MoveTo(i32, i32),
-    ChangeColorRGB(u16, u16, u16),
+    let bar = Some(4);
+    assert!(matches!(bar, Some(x) if x> 2));
 }
